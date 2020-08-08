3c62c2a8c7fd4e14edf4b1e4eae38b6b7efb25c5
erreinoso <eramirezreinoso@gmail.com>
2020-08-08 18:21:58 +0200

    run docs

---
+++ docs/precache-manifest.da2d087601f5ac0415e65a93258d8eb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cd7eb03e3c6a330283547a501efc7a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0fe2e35d37b84b11498c",
+    "url": "./static/css/main.cb357f5d.chunk.css"
+  },
+  {
+    "revision": "deb98d28c92193376be5",
+    "url": "./static/js/2.91e2304b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.91e2304b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe2e35d37b84b11498c",
+    "url": "./static/js/main.f619c8f5.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  },
+  {
+    "revision": "c880d69f853a3e937da8de0caabea161",
+    "url": "./static/media/MainBackground.c880d69f.jpg"
+  },
+  {
+    "revision": "e8c11f0802f61310a6e4547ee673b3d1",
+    "url": "./static/media/RickAndMorty33.e8c11f08.jpg"
+  }
+]);
